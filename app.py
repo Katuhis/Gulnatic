05cb7ad8d898ecc805bd6bf8fb12d8c7f502438e
Katuhis <99406064+Katuhis@users.noreply.github.com>
2022-10-31 17:32:23 +0100

    Feature 1 (#2)
    * First run
    
    * First run
    
    * First run
    
    Co-authored-by: Kate Che <43439123+CrazyDi@users.noreply.github.com>

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__, template_folder='public')
+
+
+@app.route("/admin")
+def admin():
+    return render_template('admin//index.html')
+
+
+@app.route("/")
+def home():
+    return render_template('app//home.html')
+
+
