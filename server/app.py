79657cd64595ec83e16ff0a5169a899cda5ad6ef
Katuhis <99406064+Katuhis@users.noreply.github.com>
2022-12-20 16:25:01 +0100

    Split static files (#16)

--- server/app.py
+++ server/app.py
@@ -1,16 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Blueprint
 
 
 def create_app():
     app = Flask(__name__, template_folder='../public')
     app.config.from_object("config.DevelopmentConfig")
 
-    @app.route("/admin")
+    blueprint_app = Blueprint('app', __name__, static_folder='../public/app/assets')
+    blueprint_admin = Blueprint('admin', __name__, static_folder='../public/admin/assets', url_prefix='/admin')
+
+    @blueprint_app.route("/")
+    def home():
+        return render_template('app/index.html')
+
+    @blueprint_admin.route("/")
     def admin():
         return render_template('admin/index.html')
 
-    @app.route("/")
-    def home():
-        return render_template('app/index.html')
+    app.register_blueprint(blueprint_app)
+    app.register_blueprint(blueprint_admin)
 
     return app
