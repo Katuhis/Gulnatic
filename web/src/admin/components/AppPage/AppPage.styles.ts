715bd865a207da64b27e778b386da1b8b32935d6
Katuhis <katuhis82@gmail.com>
2023-01-25 16:33:31 +0100

    Making first page

---
+++ web/src/admin/components/AppPage/AppPage.styles.ts
@@ -0,0 +1,13 @@
+import { createUseStyles } from 'react-jss'
+
+export default createUseStyles({
+  appPage: {
+    maxWidth: 1440,
+    margin: '0 auto'
+  },
+  header: {
+    padding: '24px 8px 24px 8px',
+    display: 'flex',
+    justifyContent: 'space-between'
+  }
+})
--- web/src/admin/interfaces/IPatch.ts
+++ web/src/admin/interfaces/IPatch.ts
@@ -1,3 +1,3 @@
 export default interface IPatch {
-  id: string
+  number: string
 }
